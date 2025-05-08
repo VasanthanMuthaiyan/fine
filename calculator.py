df6f3dba8fc5ae708062fde6b6e9c1517291e42c
Vasanthan M <vasanth03112@gmail.com>
2025-05-08 15:38:31 +0530

    Created calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def add(self,a,b):
+        print("Addition Of Two Numbers:",a+b)
+    def sub(self,a,b):
+        print("Subtraction Of Two Numbers:", a - b)
+    def mul(self,a,b):
+        print("Multiplication Of Two Numbers:", a * b)
+    def div(self,a,b):
+        print("Divide Of Two Numbers:", a / b)
+obj=Calculator()
+obj.add(232,534)
+obj.sub(54,564)
+obj.mul(44,43)
+obj.div(64,23)
